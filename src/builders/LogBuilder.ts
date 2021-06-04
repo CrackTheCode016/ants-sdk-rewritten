95d7eac220f179a47d3f9af21b9138a61effd73f
Bader Youssef <bader@iodlt.com>
2021-06-04 12:19:11 -0400

    added state change tx

--- src/builders/LogBuilder.ts
+++ src/builders/LogBuilder.ts
@@ -20,12 +20,22 @@
 import {
   Account,
   Address,
+  Convert,
   Deadline,
+  KeyGenerator,
+  NamespaceId,
+  NamespaceMetadataTransaction,
   NetworkType,
   PlainMessage,
+  PublicAccount,
   TransferTransaction,
 } from "symbol-sdk";
-import { REPORT_NAME_PREFIX } from "../constants";
+import {
+  CONTAINER_STATE,
+  REPORT_NAME_PREFIX,
+  SCHEMA_NAME_PREFIX,
+} from "../constants";
+import { ContainerState, ContainerStatus } from "../models/ContainerState";
 import { DataLog } from "../models/Log";
 import { BaseTransactionBuilder, WatchableTransaction } from "./BaseBuilder";
 
@@ -59,7 +69,6 @@ export class LogBuilder {
     );
     const dataChunked = log.chunk();
     dataChunked.forEach((c) => {
-      console.log(c);
       baseBuilder.add(
         TransferTransaction.create(
           Deadline.create(this.epoch),
@@ -74,5 +83,82 @@ export class LogBuilder {
 
     return baseBuilder.compile(sender, this.epoch, this.generationHash);
   }
-  //   public createLogWithStateChange(): WatchableTransaction {}
+  public createLogWithStateChange(
+    name: string,
+    log: DataLog,
+    sender: Account,
+    recipient: Address,
+    targetPublicAccount: PublicAccount,
+    cosignatories: PublicAccount[],
+    oldState: ContainerState,
+    containerStatus: ContainerStatus
+  ): WatchableTransaction {
+    const baseBuilder = new BaseTransactionBuilder(
+      this.ip,
+      false,
+      this.networkType
+    );
+    baseBuilder.add(
+      TransferTransaction.create(
+        Deadline.create(this.epoch),
+        recipient,
+        [],
+        PlainMessage.create(REPORT_NAME_PREFIX + log.schemaName),
+        this.networkType
+      ),
+      sender.publicAccount
+    );
+
+    const dataChunked = log.chunk();
+    dataChunked.forEach((c) => {
+      baseBuilder.add(
+        TransferTransaction.create(
+          Deadline.create(this.epoch),
+          recipient,
+          [],
+          PlainMessage.create(c),
+          this.networkType
+        ),
+        sender.publicAccount
+      );
+    });
+
+    baseBuilder.add(
+      this.createContainerState(
+        name,
+        targetPublicAccount,
+        oldState,
+        containerStatus
+      ),
+      targetPublicAccount
+    );
+
+    return baseBuilder.compile(sender, this.epoch, this.generationHash);
+  }
+
+  private createContainerState(
+    name: string,
+    targetPublicAccount: PublicAccount,
+    oldState: ContainerState,
+    containerStatus: ContainerStatus
+  ): NamespaceMetadataTransaction {
+    const timestamp = new Date();
+    const newState = new ContainerState(timestamp, "", containerStatus);
+    const key = KeyGenerator.generateUInt64Key(CONTAINER_STATE);
+    const newValueBytes = Convert.utf8ToUint8(JSON.stringify(newState.toDTO()));
+    const currentValueBytes = Convert.utf8ToUint8(
+      JSON.stringify(oldState.toDTO())
+    );
+    const id = new NamespaceId(name);
+
+    return NamespaceMetadataTransaction.create(
+      Deadline.create(this.epoch),
+      targetPublicAccount.address,
+      key,
+      id,
+      newValueBytes.length - currentValueBytes.length,
+      Convert.decodeHex(Convert.xor(currentValueBytes, newValueBytes)),
+      this.networkType
+    );
+  }
 }
--- test/builders/log.builder.spec.ts
+++ test/builders/log.builder.spec.ts
@@ -1,6 +1,17 @@
 import { expect } from "chai";
-import { Account, NetworkType, TransferTransaction } from "symbol-sdk";
+import {
+  Account,
+  NamespaceMetadataTransaction,
+  NetworkType,
+  Transaction,
+  TransactionType,
+  TransferTransaction,
+} from "symbol-sdk";
 import { LogBuilder } from "../../src/builders/LogBuilder";
+import {
+  ContainerState,
+  ContainerStatus,
+} from "../../src/models/ContainerState";
 import { DataLog } from "../../src/models/Log";
 
 interface Model {
@@ -50,6 +61,41 @@ describe("Log Transaction Builder Test", () => {
   });
 
   it("should create a log transaction with a state change", () => {
-    expect(true).to.be.equal(true);
+    const logBuilder = new LogBuilder(
+      "http://localhost:3000",
+      NetworkType.TEST_NET,
+      0,
+      "B626827FBD912D95931E03E9718BFE8FFD7D316E9FBB5416ED2B3C072EA32406"
+    );
+
+    const oldState = new ContainerState(new Date(), "", ContainerStatus.LOCKED);
+
+    const logWithStateChange = logBuilder.createLogWithStateChange(
+      "container",
+      testLog,
+      targetAccount,
+      targetAccount.address,
+      targetAccount.publicAccount,
+      [],
+      oldState,
+      ContainerStatus.ACTIVE
+    );
+
+    const underlying =
+      logWithStateChange.underlyingTransaction.innerTransactions;
+
+    const headTransaction = underlying[0] as TransferTransaction;
+    const logContent = underlying[1] as TransferTransaction;
+    const stateChange = underlying[2] as NamespaceMetadataTransaction;
+
+    const log = DataLog.fromDTO(JSON.parse(logContent.message.payload));
+
+    expect(logWithStateChange.underlyingTransaction.type).to.be.equal(
+      TransactionType.AGGREGATE_BONDED
+    );
+    expect(underlying.length).to.be.greaterThan(2);
+    expect(JSON.stringify(log.data)).to.be.equal(JSON.stringify(reportData));
+    expect(headTransaction.message.payload).to.be.equal("REPORT-schema");
+    expect(stateChange.type).to.be.equal(TransactionType.NAMESPACE_METADATA);
   });
 });
